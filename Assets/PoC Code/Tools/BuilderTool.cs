053a8fc5e6160f161be799b04c7277b4e05984fb
rpettefar <rpettefar@gpslsolutions.com>
2014-03-03 14:11:04 +0000

    Added a nice pixel shader for use with the building footprint. Started work on a switch-building function for preparing a footprint from a building model.

--- Assets/PoC Code/Tools/BuilderTool.cs
+++ Assets/PoC Code/Tools/BuilderTool.cs
@@ -4,56 +4,31 @@ using System.Collections;
 // A tool for placing buidings in the world
 public class BuilderTool : PointerTool
 {
-	public GameObject currentBuilding;
+	public GameObject currentBuilding;	// The currently selected building to place
+	public bool siteClear;				// If the proposed site of building contruction is clear or not
+
+	private validFootprintColour;
+	private invalidFootprintColour;
 	
 	// Constructor
 	public BuilderTool()
 	{
 		name = "Builder Tool";
+		siteClear = true;
+		validFootprintColour = Color.green;
+		validFootprintColour.a = 0.5f;
+		invalidFootprintColour = Color.red;
+		invalidFootprintColour.a = 0.5f;
 		
 		// Set the wood shack as the default start building
 		currentBuilding = GameObject.Instantiate(Resources.Load("PoC Prefabs/LoggingShed")) as GameObject;
 
 		Deactivate();
 	}
-	
-	/*public override void SwitchTo()
-	{
-		// Turn on terrain passthrough
-	}
-	
-	public override void SwitchAway()
-	{
-		// Turn off terrain passthrough
-	}
-		
-	// A general click command
-	public override void Click(GameObject gameObject, Vector3 position, bool rightClick)
-	{
-		// Check for left-click
-		if(!rightClick)
-		{
-			Quaternion rotation = Quaternion.AngleAxis(270, Vector3.right);
-			//rotation
-			//rotation.Set(270, 0, 0, 1);
-			GameObject testHouse = GameObject.Instantiate(Resources.Load("Thatched cottages/Thatched cottages"), position, Quaternion.AngleAxis(270, Vector3.right)) as GameObject;
-			//testHouse.transform.rotation.Set(270, 0, 0, 0);
-		}
-	}
-	
-	public override void MouseMove(GameObject gameObject, Vector3 position)
-	{
-		currentBuilding.transform.position = position;
-	}
-	
-	public override void MouseExit(GameObject gameObject, Vector3 position)
-	{
-		
-	}*/
 
 	public override void Activate()
 	{
-		currentBuilding.renderer.material.color = Color.green;
+		currentBuilding.renderer.material.color = validFootprintColour;
 	}
 
 	public override void Deactivate()
@@ -66,8 +41,11 @@ public class BuilderTool : PointerTool
 		// Move the footprint
 		currentBuilding.transform.position = terrainPoint;
 
+		// Check if the site is clear
+		//currentBuilding.collider.
+
 		// Check for a click
-		if(Input.GetMouseButtonUp(0)) // Left click
+		if(Input.GetMouseButtonUp(0) && siteClear) // Left click
 		{
 			// Create a new building
 			Quaternion rotation = Quaternion.AngleAxis(270, Vector3.right);
@@ -77,5 +55,19 @@ public class BuilderTool : PointerTool
 		{
 			// Switch back to select tool
 		}
+	}
+
+	// Function to switch the currently selected building and set up its shader and collider
+	public void SelectNewBuilding(GameObject building)
+	{
+		// Set the current building
+		currentBuilding = building;
+
+		// Set the shader
+		currentBuilding.renderer.material.color = validFootprintColour;
+		currentBuilding.renderer.material.shader = Shader.Find("Custom/Footprint");
+
+		// Set the collider
+		currentBuilding.AddComponent("BoxCollider");
 	}
 }
