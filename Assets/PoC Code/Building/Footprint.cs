1738157431f643765831d7bdb85a6b4103ea4b8f
rpettefar <rpettefar@gpslsolutions.com>
2014-03-06 14:51:20 +0000

    Moving functionality from Building.cs to Footprint.cs

--- Assets/PoC Code/Building/Footprint.cs
+++ Assets/PoC Code/Building/Footprint.cs
@@ -3,10 +3,51 @@ using System.Collections;
 
 public class Footprint : MonoBehaviour
 {
+	public enum Mode
+	{
+		VALID_FOOTPRINT,
+		INVALID_FOOTPRINT,
+		NORMAL,
+	}
+
+	public Mode mode;
+	private Color validFootprintColour = Color.green;
+	private Color inValidFootprintColour = Color.red;
+	private MeshFilter meshFilter;
+	private Shader defaultShader;
+	private Color defaultColor;
 	public int collidedObjects;		// Number of objects the footprint is interacting with
-	public Ray[] cornerRays;
-	public GameObject[] samplePoints;
 	public bool tooSteep;
+
+	void Awake()
+	{
+		meshFilter = GetComponentInChildren<MeshFilter>();
+		defaultShader = meshFilter.renderer.material.shader;
+		defaultColor = meshFilter.renderer.material.color;
+	}
+	
+	public void SetNormal()
+	{
+		mode = Mode.NORMAL;
+		meshFilter.renderer.material.shader = defaultShader;
+		meshFilter.renderer.material.color = defaultColor;
+		
+	}
+
+	public void SetValidFootPrint()
+	{
+		mode = Mode.VALID_FOOTPRINT;
+		meshFilter.renderer.material.shader = Shader.Find("Custom/Footprint");
+		meshFilter.renderer.material.color = validFootprintColour;
+		
+	}
+	
+	public void SetInValidFootPrint()
+	{
+		mode = Mode.INVALID_FOOTPRINT;
+		meshFilter.renderer.material.shader = Shader.Find("Custom/Footprint");
+		meshFilter.renderer.material.color = inValidFootprintColour;
+	}
 
 	// Use this for initialization
 	IEnumerator Start () 
@@ -14,32 +55,24 @@ public class Footprint : MonoBehaviour
 		// These two bits used to ensure the trigger is set. 
 		yield return new WaitForFixedUpdate();
 		collider.isTrigger = true;
-
-		//collidedObjects = -1;	// Take into account terrain collision
+		
 		collidedObjects = 0;
 
 		// Get the bottom corners of the bounding box
-		Vector3[] corners = new Vector3[4];
-		corners[0] = new Vector3(collider.bounds.center.x - collider.bounds.extents.x, collider.bounds.center.y - collider.bounds.extents.y, collider.bounds.center.z - collider.bounds.extents.z);
+		//Vector3[] corners = new Vector3[4];
+		//corners[0] = new Vector3(collider.bounds.center.x - collider.bounds.extents.x, collider.bounds.center.y - collider.bounds.extents.y, collider.bounds.center.z - collider.bounds.extents.z);
 
-		cornerRays = new Ray[4];
-		BoxCollider box = GetComponent("BoxCollider") as BoxCollider;
-		cornerRays[0].origin = box.bounds.max;
-		cornerRays[1].origin = box.bounds.min;
-
-		//Debug.Log("Max bounds: " + box.bounds.max);
-		//Debug.Log("Min bounds: " + box.bounds.min);
+		//cornerRays = new Ray[4];
+		//BoxCollider box = GetComponent("BoxCollider") as BoxCollider;
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
-		//Debug.Log("Trigger Enter");
 		collidedObjects++;
 	}
 
 	void OnTriggerExit(Collider other)
 	{
-		//Debug.Log("Trigger Exit");
 		collidedObjects--;
 	}
 
@@ -50,5 +83,56 @@ public class Footprint : MonoBehaviour
 			return true;
 		else
 			return false;
+	}
+
+	public bool CheckSite()
+	{
+		
+		if (meshFilter == null)
+			return false;
+		var mesh = meshFilter.mesh;
+		if (mesh == null)
+			return false;
+		
+		var t = meshFilter.transform;
+		var min = mesh.bounds.min;
+		var max = mesh.bounds.max;
+		
+		var corners = new Vector3[]
+		{
+			t.TransformPoint(new Vector3(min.x, min.y, min.z)),
+			t.TransformPoint(new Vector3(min.x, min.y, max.z)),
+			t.TransformPoint(new Vector3(max.x, min.y, min.z)),
+			t.TransformPoint(new Vector3(max.x, min.y, max.z)),
+			t.TransformPoint(new Vector3(0, min.y, 0)),
+		};
+		
+		int groundLayer = 1 << LayerMask.NameToLayer("Ground");
+		for (int i = 0; i < corners.Length; i++)
+		{
+			RaycastHit h;
+			var startPos = corners[i] + Vector3.up * 10f;
+			if (Physics.Raycast(new Ray(startPos, Vector3.down), out h, 1000f, groundLayer))
+			{
+				Debug.DrawLine(startPos, h.point);
+				corners[i] = h.point;
+			}
+		}
+		
+		const float MAX_INCLINE = 0.1f;
+		for (int i = 0; i < corners.Length; i++)
+		{
+			for (int j = 0; j < corners.Length; j++)
+			{
+				if (i == j)
+					continue;
+				float incline = Mathf.Abs((corners[i] - corners[j]).normalized.y);
+				Debug.Log("incline " + incline);
+				if ( incline > MAX_INCLINE)
+					return false;
+			}
+		}
+		
+		return true;
 	}
 }
--- Assets/PoC Code/Tools/BuilderTool.cs
+++ Assets/PoC Code/Tools/BuilderTool.cs
@@ -4,22 +4,21 @@ using System.Collections;
 // A tool for placing buidings in the world
 public class BuilderTool : PointerTool
 {
-	public Building currentBuilding;
-	// The currently selected building to place
-	public bool siteClear;
-	// If the proposed site of building contruction is clear or not
-	private Color validFootprintColour;
-	private Color invalidFootprintColour;
+	public GameObject currentBuilding;	// The currently selected building to place
+	public bool siteClear;				// If the proposed site of building contruction is clear or not
+	//private Color validFootprintColour;
+	//private Color invalidFootprintColour;
+
 	// Constructor
 	public BuilderTool()
 	{
 		Debug.Log("Builder tool");
 		name = "Builder Tool";
 		siteClear = false;
-		validFootprintColour = Color.green;
-		validFootprintColour.a = 0.5f;
-		invalidFootprintColour = Color.red;
-		invalidFootprintColour.a = 0.5f;
+		//validFootprintColour = Color.green;
+		//validFootprintColour.a = 0.5f;
+		//invalidFootprintColour = Color.red;
+		//invalidFootprintColour.a = 0.5f;
 
 		// Deactivate the tool
 		Deactivate();
@@ -27,20 +26,21 @@ public class BuilderTool : PointerTool
 
 	public override void Activate()
 	{
-		// Set the wood shack as the default start building (Replace this with something more suitable when this is a full working version)
-		var gm = GameObject.Instantiate(Resources.Load("PoC Prefabs/LoggingShed")) as GameObject;
+		// Set the shed as the default start building (Replace this with something more suitable when this is a full working version)
+		var gm = GameObject.Instantiate(Resources.Load("PoC Prefabs/Shack")) as GameObject;
 		var building = gm.GetComponent<Building>();
 		if (building == null)
 		{
 			Debug.LogError("prefab must have a building script attached!");
 		}
-		SelectNewBuilding(building);
+
+		// Select a new building to place
+		SelectNewBuilding(gm);
 	}
 
 	public override void Deactivate()
 	{
 		GameObject.Destroy(currentBuilding);
-		//currentBuilding.renderer.material.color = Color.clear;
 	}
 
 	public override void Update(RaycastHit target, Vector3 terrainPoint, bool hitStatus)
@@ -51,39 +51,42 @@ public class BuilderTool : PointerTool
 		// Move the footprint
 		currentBuilding.transform.position = terrainPoint;
 
-		siteClear = currentBuilding.CheckSite();
 		// Check if the site is clear
-		if (siteClear && currentBuilding.mode == Building.Mode.INVALID_FOOTPRINT)
+		siteClear = currentBuilding.CheckSite();
+
+		// Check if we can build here
+		if (!siteClear || currentBuilding.mode == Building.Mode.INVALID_FOOTPRINT)
 		{
+			// Site not clear or the terrain is too steep
 			currentBuilding.SetValidFootPrint();
 		}
-		else if(!siteClear && currentBuilding.mode == Building.Mode.VALID_FOOTPRINT)
+		else if(siteClear && currentBuilding.mode == Building.Mode.VALID_FOOTPRINT)
 		{
+			// Site clear and terrain is steep enough
 			currentBuilding.SetInValidFootPrint();
 		}
 
-
 		// Check for a click
-		if (Input.GetMouseButtonUp(0) && siteClear)
-		{ // Left click
+		if (Input.GetMouseButtonUp(0) && siteClear)	// Left click
+		{
 			// Create a new building
-			Quaternion rotation = Quaternion.AngleAxis(270, Vector3.right);
-			GameObject testHouse = GameObject.Instantiate(Resources.Load("PoC Prefabs/LoggingShed"), terrainPoint, Quaternion.AngleAxis(270, Vector3.right)) as GameObject;
+			//Quaternion rotation = Quaternion.AngleAxis(270, Vector3.right);
+			GameObject testHouse = GameObject.Instantiate(Resources.Load("PoC Prefabs/LoggingShed"), terrainPoint, Quaternion.identity) as GameObject;
 		}
 		else if (Input.GetMouseButtonUp(1))
 		{ // Right click
 			// Switch back to select tool
 		}
 	}
-
-
-
+	
 	// Function to switch the currently selected building and set up its shader and collider
-	public void SelectNewBuilding(Building building)
+	public void SelectNewBuilding(GameObject building)
 	{
 		// Set the current building
 		currentBuilding = building;
-		currentBuilding.SetValidFootPrint();
+		currentBuilding.AddComponent("Footprint");
+
+		//currentBuilding.SetValidFootPrint();
 
 	}
 }
