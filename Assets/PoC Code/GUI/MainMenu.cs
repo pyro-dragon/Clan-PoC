00418c408dff1bcc5d7fdca8d8e22ce434c813c1
rpettefar <rpettefar@gpslsolutions.com>
2014-05-27 15:12:38 +0100

    Added a new scene and the start of a game state manager. Moved about some resources too.

---
+++ Assets/PoC Code/GUI/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// GUI code
+	void onGUI()
+	{
+		if(GUI.Button (new Rect(30, 03, 150, 30), "Start Game"))
+		{
+			startGame();
+		}
+	}
+
+	private void starGame()
+	{
+		print("Starting game");
+
+		DontDestroyOnLoad(GameStateManager.Instantiate);
+		GameStateManager.Instantiate.startState();
+	}
+}
