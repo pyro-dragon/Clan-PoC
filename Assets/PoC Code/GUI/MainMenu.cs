cd7b3380bb71feacaa3bb9515958ec438d4e308b
rpettefar <rpettefar@gpslsolutions.com>
2014-05-29 14:09:53 +0100

    Set up the main menu layout

--- Assets/PoC Code/GUI/MainMenu.cs
+++ Assets/PoC Code/GUI/MainMenu.cs
@@ -6,13 +6,36 @@ public class MainMenu : MonoBehaviour {
 	// GUI code
 	void OnGUI()
 	{
-
-
-
-		if(GUI.Button (new Rect(30, 03, 150, 30), "Start Game"))
-		{
-			this.startGame();
-		}
+		// Menu area
+		GUI.BeginGroup (new Rect ((Screen.width/2) - 100, (Screen.height/2) - 200, 200, 400));
+		
+			// We'll make a box so you can see where the group is on-screen.
+			GUI.Box(new Rect (0,0,200,170), "" );
+			
+			if(GUI.Button(new Rect(10, 10, 180, 30), "New Game"))
+			{
+				//this.gameScreenManager.SetScreen("newGameScreen");
+				this.startGame();
+			}
+			
+			if(GUI.Button(new Rect(10, 50, 180, 30), "Load Game"))
+			{
+				//this.gameScreenManager.SetScreen("activityScreen");
+			}
+			
+			if(GUI.Button(new Rect(10, 90, 180, 30), "Options"))
+			{
+				//this.gameScreenManager.SetScreen("activityMenu");
+			}
+			
+			if(GUI.Button(new Rect(10, 130, 180, 30), "Exit"))
+			{
+				//gameScreenManager.LogData("Quitting");
+				Application.Quit();
+			}
+		
+		// End the group we started above. This is very important to remember!
+		GUI.EndGroup ();
 	}
 
 	private void startGame()
