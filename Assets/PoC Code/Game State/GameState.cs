73f461ecc77851deeef3080e3753f32f0b3a5933
rpettefar <rpettefar@gpslsolutions.com>
2014-06-26 15:08:00 +0100

    Changing state layout

--- Assets/PoC Code/Game State/GameState.cs
+++ Assets/PoC Code/Game State/GameState.cs
@@ -4,22 +4,24 @@ using System.Collections;
 // A state for the game to be in
 public class GameState 
 {
-	private string stateName;	// The name of the state
+	private string stateName;			// The name of the state
+	protected GameScreen currentScreen;	// The game screen to display
 
 	// Constructor
 	public GameState(string name)
 	{
 		stateName = name;
+		currentScreen = null;
 	}
 
-	// A function to perform just before this state is transitioned away from
-	public virtual void TransitioningAway(GameState nextState)
+	// A function to perform when the state begins
+	public virtual void BeginState(GameState lastState)
 	{
 		
 	}
 
-	// A function to perform before the state is transitioned into
-	public virtual void TransitioningTo(GameState previouseState)
+	// A function to perform when the state end
+	public virtual void EndState(GameState nextState)
 	{
 
 	}
--- Assets/PoC Code/Game State/NoGame.cs
+++ Assets/PoC Code/Game State/NoGame.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// A game state when no game is currently in progress. This is typically used to display the main menu before a game begins and after a game has been quit. 
 public class NoGame : GameState 
 {
+	// Constructor
 	public NoGame() : base("NoGame")
 	{
+		currentScreen = GameScreenManager
+	}
 
+	// Start state
+	public override void BeginState (GameState lastState)
+	{
+		base.BeginState (lastState);
 	}
 }
